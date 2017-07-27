6d4013cf91f172396f2463c693eae8365c0c27da
AinL <gmlwns5176@gmail.com>
2017-07-27 22:19:28 +0900

    초기 커밋.

---
+++ PexelEngine.Launcher/PexelEngine.Launcher/Core.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace PexelEngine.Launcher
+{
+    public abstract class Core
+    {
+        public static Core Current { get; private set; }
+
+        public static void Init(Core core)
+        {
+            Current = core;
+        }
+    }
+}
